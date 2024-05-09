05e735c84d845b43005b28f386e82d39f885d319
Trevor Pendras <trevor@pendras.com>
2024-05-09 00:51:43 -0700

    FISH-executable script, absolute paths

--- seasports.R
+++ seasports.R
@@ -8,4 +8,4 @@ url <- "https://www.seattletimes.com/nation-world/sports-on-tv-radio-2/"
 # Run function
 time_sports_table <- seasports_extract(url)
 
-kable(time_sports_table, format = "pipe")
+kable(time_sports_table, format = "rst")
